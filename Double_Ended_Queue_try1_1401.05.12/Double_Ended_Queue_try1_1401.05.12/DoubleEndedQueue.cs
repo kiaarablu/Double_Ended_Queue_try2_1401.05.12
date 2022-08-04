0f4be3f72b8d22f7f8750df84376ca69f836482b
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-04 11:19:11 +0430

    Add files via upload

---
+++ Double_Ended_Queue_try1_1401.05.12/Double_Ended_Queue_try1_1401.05.12/DoubleEndedQueue.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Ended_Queue_try1_1401._05._12
+{
+    internal class DoubleEndedQueue
+    {
+        int[] DoubleQueue ;
+        int index1 = -1;
+        int index2 ;
+        
+        public DoubleEndedQueue(int length)
+        {
+            DoubleQueue = new int[length];
+            index2 = DoubleQueue.Length;
+        }
+
+        public bool isfull()
+        {
+            if (index1 + 1 == index2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isempty()
+        {
+            if (index2 == DoubleQueue.Length)
+            {
+                return true ;
+            }
+            else
+            {
+                return false ;
+            }
+
+
+        }
+        public void index1_enqueue(int value)
+        {
+            if (!isfull())
+            {
+                index1++;
+                DoubleQueue[index1] = value;
+            }
+            else
+            {
+                Console.WriteLine("Array is full...!");
+            }
+        }
+        public void index1_dequeue()
+        {
+            if (!isempty())
+            {
+                if (index1 == -1)
+                {
+                    Console.WriteLine("empty");
+                }
+                else
+                {
+                Console.WriteLine(DoubleQueue[index1]);
+                index1--;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Array is empty...!");
+            }
+        }
+        public void index2_enqueue(int value2)
+        {
+            
+            if (!isfull())
+            {
+                index2--;
+                DoubleQueue[index2] = value2;
+            }
+            else
+            {
+                Console.WriteLine("Array is full...!");
+            }
+        }
+        public void index2_dequeue()
+        {
+            if (!isempty())
+            {
+                Console.WriteLine(DoubleQueue[index2]);
+                index2++;
+            }
+            
+            else
+            {
+                Console.WriteLine("Array is empty...!");
+            }
+        }
+        public void print_array()
+        {
+            
+            
+            if (!isempty())
+            {
+                for (int i = 0; i <= index1; i++)
+                {
+                    int in1p;
+                    in1p = DoubleQueue[i];
+                    Console.Write(in1p);
+                }
+                for (int j =index2 ; j < DoubleQueue.Length; j++)
+                {
+                    int in2p;
+                    in2p = DoubleQueue[j];
+                    Console.Write(in2p);
+                }
+                
+
+            }
+
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ Double_Ended_Queue_try1_1401.05.12/Double_Ended_Queue_try1_1401.05.12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Ended_Queue_try1_1401._05._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleEndedQueue a = new DoubleEndedQueue(10);
+            a.index1_enqueue(20);
+            a.index1_enqueue(30);
+            a.index2_enqueue(400);
+            a.index2_enqueue(45);
+            a.index2_enqueue(450);
+            Console.WriteLine("-------------------------");
+            a.print_array();
+            Console.WriteLine("-------------------------");
+
+
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_enqueue(500);
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index2_dequeue();
+            a.index1_dequeue();
+            a.index1_dequeue();
+            a.index1_dequeue();
+            a.index1_enqueue(500);
+            a.index1_dequeue();
+            a.index1_dequeue();
+            a.index1_dequeue();
+            a.index2_enqueue(800);
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
